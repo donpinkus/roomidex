088b56c4664284f1f9cef2c1efe1f90b532464ef
Don Pinkus <don.pinkus@gmail.com>
2014-01-27 21:38:00 -0800

    fixing heroku fb login 3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'OMNIAUTH_PROVIDER_KEY', 'OMNIAUTH_PROVIDER_SECRET'
+  provider :facebook, '475554302502752', '602e200c4df42e11f9327ce7f4ba28ce'
 end
