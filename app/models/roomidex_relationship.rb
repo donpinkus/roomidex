7151b177e2f2db28e2e533bae5535cb41af0c578
Don Pinkus <don.pinkus@gmail.com>
2014-03-16 22:13:43 -0700

    added edge getters for roomidex relationships

--- app/models/roomidex_relationship.rb
+++ app/models/roomidex_relationship.rb
@@ -2,4 +2,21 @@ class RoomidexRelationship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
   validates_uniqueness_of :user_id, :scope => :friend_id
+
+  def self.get_edge(first_id, second_id)
+  	edge = self.get_directed_edge(first_id, second_id)
+  	
+  	if edge.size == 0 
+  		edge = self.get_directed_edge(second_id, first_id)
+  	end
+
+  	edge
+  end
+
+  def self.get_directed_edge(first_id, second_id)
+  	edge = RoomidexRelationship.where(
+  		user_id: first_id,
+  		receiver_id: second_id
+	  )
+  end
 end
